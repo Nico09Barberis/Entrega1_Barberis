e1f07fc8c258e7def01683c8afaa262833467928
Nico Barberis <nico09barberis@gmail.com>
2022-07-31 17:56:32 -0300

    creacion de avatar correcto

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from Accounts.models import MasDatosUsuario
+
+admin.site.register(MasDatosUsuario)
--- Accounts/forms.py
+++ Accounts/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -9,10 +10,19 @@ class MyUserCretionForm(UserCreationForm):
     password1 = forms.CharField(label='password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='repetir password', widget=forms.PasswordInput)
     
-    first_name = forms.CharField(label='nombre', max_length=30)
-    last_name = forms.CharField(label='apellido', max_length=30)
+ 
     
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
         help_texts = { key: '' for key in fields}
+
+
+class MyUserEditForm(forms.Form):
+    email = forms.EmailField(required=False)
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput, required=False)
+    password2 = forms.CharField(label='repetir password', widget=forms.PasswordInput, required=False)
+    first_name = forms.CharField(label='nombre', max_length=30, required=False)
+    last_name = forms.CharField(label='apellido', max_length=30, required=False)
+    avatar = forms.ImageField(required=False)
+    
